7521711eff73f080f7f35915a6fdd1e734ef91f0
Buse Sarısoy <busesarisoy@gmail.com>
2019-11-15 00:57:31 +0300

    ekleme

---
+++ 2.js
@@ -0,0 +1,7 @@
+var numbers = [1, 4, 9];
+
+var result = numbers.map(function (index) {
+  return index*3;
+})
+
+console.log(result);
---
+++ 3.js
@@ -0,0 +1,7 @@
+var numbers = [12, 5, 20, 130, 44, 9, 240]
+
+var result = numbers.filter(function (index) {
+  return index<15;
+})
+
+console.log(result);
